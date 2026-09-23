1337fa94db7902d32bc8d3ada0113fc2e8c50640
Sam-2610 <satyamsagar827@gmail.com>
2026-09-23 10:27:08 +0530

    Commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -13,12 +13,12 @@ my_post = [{"title":"favorite food", "content":"I like Pizza", "id":2}]
 
 @router.get("/", response_model=List[schemas.PostResponse])
 def get_posts(db:Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(models.Post.user_id == user_id.id).all()
     return posts
 
 @router.post("/", status_code= status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_post(post:schemas.CreatePost, db:Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
-   new_post = models.Post(**post.dict())
+   new_post = models.Post(user_id = user_id.id, **post.dict())
    db.add(new_post)
    db.commit()
    db.refresh(new_post)
@@ -45,15 +45,23 @@ def get_post(id:int, response:Response, db:Session = Depends(get_db), user_id : 
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int, db:Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
 
-    if post.first() == None:
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(
             status_code= status.HTTP_404_NOT_FOUND,
             detail= f"post with {id} doesnt exist"
         )
 
-    post.delete(synchronize_session= False)
+    if post.user_id != user_id.id:
+        raise HTTPException(
+            status_code= status.HTTP_403_FORBIDDEN,
+            detail="Not Authorised for requested Action"
+        )
+    
+    post_query.delete(synchronize_session= False)
     db.commit()
     return Response(status_code= status.HTTP_204_NO_CONTENT)
 
@@ -67,6 +75,12 @@ def update_post(id:int, updated_post:schemas.CreatePost, db:Session = Depends(ge
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND,
             detail = f"post with {id} doesnt exist"
+        )
+
+    if post.user_id != user_id.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not Authorised For Requested Action"
         )
 
     post_query.update(updated_post.dict(), synchronize_session= False)
